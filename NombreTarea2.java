1960c74b9c01a8b47461e4b3e633c5709a03e954
Melicita014 <145325113+Melicita014@users.noreply.github.com>
2023-12-20 20:01:37 +0100

    Nombre2.java

--- NombreTarea2.java
+++ NombreTarea2.java
@@ -1,50 +1,22 @@
 //¿Cúal de las cadenas de texto es alfabéticamente la primera? ¿En qué posición está? 
 //Con los datos del ejemplo la respuesta sería:Anna, en posición 2. Cadena(0).compareTo(cadena(1))
-public class NombreTarea2 {
+public class Nombre2 {
 
 	public static void main(String[] args) {
-		String Nombres[] = {"Sue","Charlotte","Anna","Mike","William","Ed",};//ACEMSW		
-		String Guardado = Nombres[0];
-		int pos = 0 ;
-		
-
-		for (int i=0;i<Nombres.length-1;i++) {
-			if (Nombres[i].compareTo(Guardado)<0  ) {
-				Guardado = Nombres[i];
-				pos = i ;
-			}}
-			
-	System.out.println("La primera alfabeticamente es: " + Guardado + " Y esta en la posicion "+pos);	
-	
-
-	}}
-		/*for (int i=0;i<Nombres.length-1;i++) {
-            if (Nombres[i].compareTo(Nombres[i+1])<0 ){
-                String temp = Nombres[i];
-                Nombres[i]= Nombres[i+1];
-                 Nombres[i+1] = temp ;   
-              
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
-             System.out.println(temp) ;     
-        }
-		
-	
-
-		
-	
-			
-
+				String Nombres[] = {"Sue","Charlotte","Anna","Mike","William","Ed",};//ACEMSW		
+				String Guardado = Nombres[0];// Variable para poder comparar.
+				int pos = 0 ; // Variable para poder saber luego la posicion.
+				
+// Bucle para hacer la comparacion
+				for (int i=0;i<Nombres.length-1;i++) {
+					if (Nombres[i].compareTo(Guardado)<0  ) {
+						Guardado = Nombres[i];// Cuando confirma que esta primero alfabeticamente sustituye el que estaba. 
+						pos = i ; // Y luego sustituye la posicion
+					}
+				}	
+				//Mensaje fuera del bucle
+			System.out.println("La primera alfabeticamente es: " + Guardado + " Y esta en la posicion "+pos);
+				
 	}
-*/
 
-	
-/*		for (int i=0;i<Nombres.length-1;i++) {
-			if (Nombres[i].compareTo(Nombres[i+1])==0){
-				primero = Nombres[i]; 
-			                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
-			}	
-			String Origen = "Sue" ;
-		for (int i=0;i<Nombres.length;i++)
-			if (Nombres[2].charAt(0)>= 'A' && Nombres[2].charAt(0)<='Z')
-				;
-	*/
+}
