473cc3c21d032209a4d7f5e95cc48c2bdc20ee07
Melicita014 <145325113+Melicita014@users.noreply.github.com>
2023-12-20 22:04:14 +0100

    Add files via upload

---
+++ Nombre3.java
@@ -0,0 +1,26 @@
+//¿Cuántas letras mayúsculas hay en cada posición del array? 
+
+public class Nombre3 {
+	public static void main(String[] args) {
+		String Nombres[] = {"SuE","ChARlotte","ANNa","MIke","WILLIam","ed",};
+		int contador = 0 ;
+		
+		for (int i=0;i<Nombres.length;i++) {
+			contador = 0 ; // Lo pongo a 0 para el siguiente nombre
+			for (int j=0;j<Nombres.length;j++) {	
+				if (j < Nombres[i].length()) { // Compruebo si el indice de caracter  es menor que la longitud del nombre. Sino da error.
+			
+				if (Nombres[i].charAt(j)>=65 && Nombres[i].charAt(j)<=90) { // Codigos ASCII A = 65 Hasta Z=90
+					contador++ ;
+					
+				}
+				
+				}
+			}
+			System.out.println (Nombres[i]  + ": " + contador);
+		
+		}
+		
+
+    }
+}
